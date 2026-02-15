8c6d94e0c4a77262ba221e3f35afa84e526d3b96
dausmhf <muh.fir.firdaus@gmail.com>
2026-02-15 19:46:07 +0700

    Define a Variable and Reassing variable

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-// Greeting Message
-console.log('Hello, World!'); 
+// Define a variable to hold the name
+let userName = "alice";
+// Greeting Message for define variable
+console.log("Hello " + userName)
+
+// Reassign the variable to a new name
+userName = "bob";
+// Greeting Message for reassign variable
+console.log("Hello " + userName)
